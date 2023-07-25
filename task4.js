8a0f87e2a49d2a31b5a51ca78a73e36b6c4f0130
developer-shourav <mrinmoyraj823@gmail.com>
2023-07-26 02:16:56 +0600

    4th init complete

--- task4.js
+++ task4.js
@@ -4,4 +4,28 @@ Task 4: Create a function that takes a sorted array of numbers and a target valu
  Example Input: ([1, 3, 6, 8, 11, 15], 9) Example Output: [1, 2] (numbers at indices 1 and 2: 3 + 6 = 9) 
  */
 
- 
+ function findTwoNumbers(array, target) {
+    let left = 0;
+    let right = array.length - 1;
+  
+    while (left < right) {
+      const sum = array[left] + array[right];
+  
+      if (sum === target) {
+        return [left, right];
+      } else if (sum < target) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  
+    return []; // Return an empty array if no such pair is found
+  }
+  
+  // Example usage:
+  const inputArray = [1, 3, 6, 8, 11, 15];
+  const targetValue = 9;
+  const result = findTwoNumbers(inputArray, targetValue);
+  console.log(result); // Output: [1, 2] (numbers at indices 1 and 2: 3 + 6 = 9)
+  
