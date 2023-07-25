541d46bb968c2a7334eee4d706f8f47b89926faf
developer-shourav <mrinmoyraj823@gmail.com>
2023-07-25 14:03:05 +0600

    second task complete

--- task2.js
+++ task2.js
@@ -14,12 +14,24 @@ function calculatePositiveNumbers(arrayOfNumbers){
    
     let total = 0;
     for( let i = 0; i < arrayOfNumbers.length; i++){
-        if(arrayOfNumbers[i] >= 0){
+        if(arrayOfNumbers[i] > 0){
             total = total + arrayOfNumbers[i];
         }
     }
 
     return total;
-}
+};
 
 console.log(calculatePositiveNumbers([2, -5, 10, -3, 7]));
+
+/* ------------------------------------------
+      Solution Two using es6 array method:
+------------------------------------------------ */
+
+const totalOfPositiveNumbers = arrayOfNumbers => {
+    const positiveNumbers = arrayOfNumbers.filter( numbers => numbers > 0);
+    const total = positiveNumbers.reduce((previous, present) => previous + present , 0);
+    return total;
+}
+
+console.log(totalOfPositiveNumbers([2, -5, 10, -3, 7]));
